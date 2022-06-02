e3de21ef46dfad79735f21081ca0659d39bad579
cemkur <47397992+cemkur@users.noreply.github.com>
2022-06-02 06:23:26 -0700

    Update docker-portainer-omada.sh

--- docker-portainer-omada.sh
+++ docker-portainer-omada.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install ca-certificates curl gnupg lsb-release
+sudo apt install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
@@ -7,10 +7,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 sudo apt update
-sudo apt install docker-compose docker-compose-plugin
+sudo apt install docker-compose docker-compose-plugin -y
 
 read -p "Press [Enter] key to check docker compose version..."
 docker compose version
