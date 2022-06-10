2467c208b1cfecadf5b39d73c2a21bd50a5a3bb6
cemkur <47397992+cemkur@users.noreply.github.com>
2022-06-10 11:17:06 -0700

    Update docker-portainer-omada.sh

--- docker-portainer-omada.sh
+++ docker-portainer-omada.sh
@@ -7,7 +7,7 @@ sudo apt install ca-certificates curl gnupg lsb-release
 #read -p "Press [Enter] key to continue..."
 sudo mkdir -p /etc/apt/keyrings
 #read -p "Press [Enter] key to continue..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 #read -p "Press [Enter] key to continue..."
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
